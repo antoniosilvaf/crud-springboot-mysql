bf1908d94446a7c8cbf9efc8e2d4492843aa7876
Antonio Silva <antoniosergio21@hotmail.com>
2022-12-06 16:27:24 -0300

    feat: Comment endpoints

--- src/main/java/com/springcrud/springcrud/service/CommentService.java
+++ src/main/java/com/springcrud/springcrud/service/CommentService.java
@@ -1,12 +1,19 @@
 package com.springcrud.springcrud.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.springcrud.springcrud.model.Comment;
 import com.springcrud.springcrud.repository.CommentRepository;
+import com.springcrud.springcrud.service.exception.DatabaseException;
+import com.springcrud.springcrud.service.exception.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class CommentService {
@@ -16,5 +23,38 @@ public class CommentService {
 
     public List<Comment> findAll() {
         return repository.findAll();
+    }
+
+    public Comment findById(Long id) {
+        Optional<Comment> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+    }
+
+    public Comment insert(Comment obj) {
+        return repository.save(obj);
+    }
+
+    public void delete(Long id) {
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
+    }
+
+    public Comment update(Long id, Comment obj) {
+        try {
+            Comment entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
+    }
+
+    public void updateData(Comment entity, Comment obj) {
+        entity.setText(obj.getText());
     }
 }

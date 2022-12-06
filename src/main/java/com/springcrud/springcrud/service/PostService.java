4e41122f606a8928fe4ad7552e132d23f9b53a00
Antonio Silva <antoniosergio21@hotmail.com>
2022-12-06 16:19:11 -0300

    feat: Post endpoints

--- src/main/java/com/springcrud/springcrud/service/PostService.java
+++ src/main/java/com/springcrud/springcrud/service/PostService.java
@@ -1,12 +1,19 @@
 package com.springcrud.springcrud.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.springcrud.springcrud.model.Post;
 import com.springcrud.springcrud.repository.PostRepository;
+import com.springcrud.springcrud.service.exception.DatabaseException;
+import com.springcrud.springcrud.service.exception.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class PostService {
@@ -16,5 +23,39 @@ public class PostService {
 
     public List<Post> findAll() {
         return repository.findAll();
+    }
+
+    public Post findById(Long id) {
+        Optional<Post> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+    }
+
+    public Post insert(Post obj) {
+        return repository.save(obj);
+    }
+
+    public void delete(Long id) {
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
+    }
+
+    public Post update(Long id, Post obj) {
+        try {
+            Post entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
+    }
+
+    public void updateData(Post entity, Post obj) {
+        entity.setText(obj.getText());
+        entity.setTitle(obj.getTitle());
     }
 }
